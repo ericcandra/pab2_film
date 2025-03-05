00d9fa507341779a7c865dba984aaa9b43501cf6
ericcandra <ericcandra_2226240059@mhs.mdp.ac.id>
2025-03-05 09:23:22 +0700

    update

--- film/lib/models/movie.dart
+++ film/lib/models/movie.dart
@@ -12,12 +12,12 @@ class Movie {
   Movie(
       {required this.id, 
       required this.title, 
-      required this.overview, 
+      required this.overview,
       required this.posterPath, 
       required this.backdropPath, 
       required this.releaseDate, 
       required this.voteAverage});
   factory Movie.fromJson(Map<String, dynamic> json) {
-    return Movie(id: json['id'], title: json['title'], overview: json['overview'], posterPath: json['posterPath'], backdropPath: json['backdropPath'], releaseDate: json['releaseDate'], voteAverage: json['voteAverage'].toDouble());
+    return Movie(id: json['id'], title: json['title'], overview: json['overview'], posterPath: json['poster_Path'], backdropPath: json['backdrop_Path'], releaseDate: json['release_Date'], voteAverage: json['vote_Average'].toDouble());
   }      
 }
